117f0c919d9f5ceb2e78fc5c6ef33195a31cb0c8
nehnubasheer <87762100+nehnubasheer@users.noreply.github.com>
2021-07-21 21:25:23 +0530

    Add files via upload

---
+++ html/prct1.js
@@ -0,0 +1,10 @@
+Function func(){}
+	Const xhttp=new XMLHttpRequest();
+	Xhttp.onloaf=function(){
+		Document.getElementByld('A1').innerHTML=abc
+		Document.getElementByld('A2').innerHTML=abc
+		Document.getElementByld('A3').innerHTML=abc
+	}
+	Xhttp.open("GET");
+	Xhttp.sent();
+}

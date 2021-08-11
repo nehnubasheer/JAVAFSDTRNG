56f788f0061fcff307298421304ebef11ee39e3b
nehnubasheer <87762100+nehnubasheer@users.noreply.github.com>
2021-08-11 11:22:12 +0530

    Add files via upload

---
+++ SpringBootCrud/Spring-boot-crud/src/main/java/com/ust/repository/BooksRepository.java
@@ -0,0 +1,9 @@
+package com.ust.repository;
+import org.springframework.data.repository.CrudRepository;
+
+import com.ust.model.Books;
+
+
+public interface BooksRepository extends CrudRepository<Books, Integer>  {
+
+}
